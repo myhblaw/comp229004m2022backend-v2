2506e571b17e2be1f7c5494895f33ff6740f57eb
myhblaw <105828460+myhblaw@users.noreply.github.com>
2022-08-09 21:57:59 -0400

    hard mode

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-    "ATLASDB" : process.env.ATLASDB,
-    "SECRETKEY" : process.env.SECRETKEY
+    "ATLASDB" : "mongodb+srv://fixItGroup3:sellItGroup3@cluster0.omcrx.mongodb.net/?retryWrites=true&w=majority",
+    "SECRETKEY" : "secret"
 }
