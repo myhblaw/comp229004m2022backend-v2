837a3f3f929f7ffad36f1e10e3590fca9f8816a4
myhblaw <105828460+myhblaw@users.noreply.github.com>
2022-08-10 21:01:17 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-    "ATLASDB" : "mongodb+srv://fixItGroup3:sellItGroup3@cluster0.omcrx.mongodb.net/?retryWrites=true&w=majority",
-    "SECRETKEY" : "secret"
+    "ATLASDB" : process.env.ATLASDB,
+    "SECRETKEY" : process.env.SECRETKEY
 }
